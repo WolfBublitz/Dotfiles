614d22723b40aebcaf3b0c27fd60b2acc5c1e0d3
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-07 04:19:43 +0000

    added install script for oh-my-posh

---
+++ oh-my-posh/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "installing oh my posh"
+
+cd oh-my-posh
+
+OMP_DIR=~/bin
+OMP_THEMES_DIR=~/.poshthemes
+
+if [[ ! -d $OMP_DIR ]]; then
+    mkdir $OMP_DIR
+fi
+
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $OMP_DIR
+
+#$OMP_DIR/oh-my-posh font install
+
+if [[ ! -d $OMP_THEMES_DIR ]]; then
+    mkdir ~/.poshthemes
+fi
+
+cp theme.omp.json $OMP_THEMES_DIR
+
+echo eval "$($OMP_DIR/oh-my-posh init bash --config $OMP_THEMES_DIR/theme.omp.json))" >> ~/.bashrc
+
+cd ..
