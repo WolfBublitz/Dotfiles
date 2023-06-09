7d2ef360ddc81440da64299ec48ec9475dce8dfe
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-09 10:45:44 +0200

    Update install.sh

--- oh-my-posh/install.sh
+++ oh-my-posh/install.sh
@@ -6,7 +6,7 @@ cd oh-my-posh
 
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/bin
 
-if [ -x "$(command -v bash") ]; then
+if [ -x "$(command -v bash)" ]; then
     echo "Installing Oh My Posh for Bash"
     echo eval "$(oh-my-posh init bash --config ~/theme.omp.json)" >> ~/.bashrc
 fi
