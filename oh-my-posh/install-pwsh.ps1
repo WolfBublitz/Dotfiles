30f0dad21527a93c1da09b5aa75a7b78822d9cea
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-09 10:51:30 +0200

    Update install-pwsh.ps1

--- oh-my-posh/install-pwsh.ps1
+++ oh-my-posh/install-pwsh.ps1
@@ -2,4 +2,4 @@ if (!(Test-Path $PROFILE)) {
   New-Item -Path $PROFILE -Force
 }
 
-Write-Host oh-my-posh init pwsh --config ~/theme.omp.json | Invoke-Expression >> $PROFILE
+Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh --config ~/theme.omp.json | Invoke-Expression"
