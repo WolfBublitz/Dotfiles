226fd9eace50da6cb1e70bbddf6e977b4bbbfd49
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-09 11:03:47 +0200

    Update psreadline-setup.ps1

--- psreadline/psreadline-setup.ps1
+++ psreadline/psreadline-setup.ps1
@@ -1,1 +1,20 @@
+if ($host.Name -eq 'ConsoleHost' -or $host.Name -eq 'Visual Studio Code Host' ) {
+    
+    # configuring PSReadLine
+    Import-Module PSReadLine
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 
+    Set-PSReadlineOption -Color @{
+        "Command"          = [ConsoleColor]::Green
+        "Parameter"        = [ConsoleColor]::Gray
+        "Operator"         = [ConsoleColor]::Magenta
+        "Variable"         = [ConsoleColor]::Yellow
+        "String"           = [ConsoleColor]::Yellow
+        "Number"           = [ConsoleColor]::Yellow
+        "Type"             = [ConsoleColor]::Cyan
+        "Comment"          = [ConsoleColor]::DarkCyan
+        "InlinePrediction" = '#70A99F'
+    }
+}
