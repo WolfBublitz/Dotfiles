5dd5c8651685d1f273299feafee8c7b9b862000a
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-09 11:33:06 +0200

    Update psreadline-setup.ps1

--- psreadline/psreadline-setup.ps1
+++ psreadline/psreadline-setup.ps1
@@ -5,6 +5,7 @@ if ($host.Name -eq 'ConsoleHost' -or $host.Name -eq 'Visual Studio Code Host' ) 
     Set-PSReadLineOption -PredictionSource HistoryAndPlugin
     Set-PSReadLineOption -PredictionViewStyle ListView
     Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
     Set-PSReadlineOption -Color @{
         "Command"          = [ConsoleColor]::Green
