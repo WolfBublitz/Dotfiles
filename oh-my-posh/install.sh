570fa04ab0df384c0e5d263ed76742da97b90049
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-09 10:43:47 +0200

    Update install.sh

--- oh-my-posh/install.sh
+++ oh-my-posh/install.sh
@@ -4,26 +4,13 @@ echo "installing oh my posh"
 
 cd oh-my-posh
 
-OMP_DIR=~/bin
-OMP_THEMES_DIR=~/.poshthemes
-
-if [[ ! -d $OMP_DIR ]]; then
-    mkdir $OMP_DIR
-fi
-
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $OMP_DIR
-
-#$OMP_DIR/oh-my-posh font install
-
-if [[ ! -d $OMP_THEMES_DIR ]]; then
-    mkdir ~/.poshthemes
-fi
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/bin
 
 cp theme.omp.json $OMP_THEMES_DIR
 
 if [ -x "$(command -v base)" ]; then
     echo "Installing Oh My Posh for Bash"
-    echo eval "$($OMP_DIR/oh-my-posh init bash --config $OMP_THEMES_DIR/theme.omp.json)" >> ~/.bashrc
+    echo eval "$(oh-my-posh init bash --config ~/theme.omp.json)" >> ~/.bashrc
 fi
 
 if [ -x "$(command -v pwsh)" ]; then
