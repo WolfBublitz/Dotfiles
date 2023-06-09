0f4ade38218305469d4697df598e70fb2702ecba
Wolf Bublitz <w.bublitz@krohne.com>
2023-06-09 09:13:41 +0000

    updated install

--- psreadline/install-pwsh.ps1
+++ psreadline/install-pwsh.ps1
@@ -1,5 +1,11 @@
 Install-Module -Name PSReadLine -AllowClobber -Force;
 
-$filePath = $PSScriptRoot/psreadline-setup.ps1
+$profileDirPath = [System.IO.Path]::GetDirectoryName($PROFILE)
 
-Add-Content -Path $PROFILE -Value ". $(filePath)"
+if (!(Test-Item -Path $PROFILE)) {
+    New-Item -Path $PROFILE -ItemType File
+}
+
+Copy-Item -Path "$PSScriptRoot/psreadline-setup.ps1" -Destination $profileDirPath -Force
+
+Add-Content -Path $PROFILE -Value ". psreadline-setup.ps1"
