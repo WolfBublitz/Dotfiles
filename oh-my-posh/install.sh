04b6341c13fb6b1ac1c0b245c43e88c3c5a2100f
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-06-10 07:16:28 +0200

    oh my posh does now install FiraCode

--- oh-my-posh/install.sh
+++ oh-my-posh/install.sh
@@ -6,6 +6,10 @@ echo "Installing Oh My Posh"
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 # copying theme
+echo "-> copying theme"
 mkdir -p ~/.poshthemes
 cp theme.omp.json ~/.poshthemes/theme.omp.json
 
+# installing fonts
+echo "-> installing font FiraCode"
+oh-my-posh font install FiraCode
