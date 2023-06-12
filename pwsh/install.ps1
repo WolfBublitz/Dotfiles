80362711cbc7bf6c7e3ce4b4e20504fbdbbf079e
Wolf Bublitz <w.bublitz@krohne.com>
2023-06-12 04:44:32 +0000

    fixed powershell install

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,19 +1,25 @@
 Write-Host "Installing Oh My Posh for Powershell" -ForegroundColor Green
 
+$profileDirPath = [System.IO.Path]::GetDirectoryName($PROFILE)
+
 if (Test-Path $PROFILE) {
   Write-Host "-> Backing up $PROFILE to $PROFILE.bak"
   Copy-Item -Path $PROFILE -Destination "$PROFILE.bak" -Force
   Remove-Item -Path $PROFILE -Force
 }
+else {
+  Write-Host "-> Creating directory $profileDirPath"
+  New-Item -Path $profileDirPath -ItemType Directory -Force
+}
 
-$null = New-Item -Path $PROFILE -ItemType File
+Write-Host "-> Creating file $PROFILE"
+$null = New-Item -Path $PROFILE -ItemType File -Force
 
 Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh --config ~/.poshthemes/theme.omp.json | Invoke-Expression"
 
 Write-Host "-> Installing PSReadLine"
 Install-Module -Name PSReadLine -AllowClobber -Force;
 
-$profileDirPath = [System.IO.Path]::GetDirectoryName($PROFILE)
 Copy-Item -Path "$PSScriptRoot/psreadline-setup.ps1" -Destination $profileDirPath -Force
 
 Add-Content -Path $PROFILE -Value ". `"`$PSSCRIPTROOT/psreadline-setup.ps1`""
