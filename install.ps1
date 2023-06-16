3bd437dfc9ea2fb841147313a3871bf2c16509cc
Wolf Bublitz <w.bublitz@krohne.com>
2023-06-16 09:53:30 +0200

    update

--- install.ps1
+++ install.ps1
@@ -1,21 +1,23 @@
+$ohMyPoshPath = "/usr/bin"
+
 Push-Location oh-my-posh
-./install.ps1 -Path "/usr/bin"
+./install.ps1 -Path $ohMyPoshPath
 Pop-Location
 
 if (Get-Command bash -ErrorAction SilentlyContinue) {
     Push-Location bash
-    ./install.ps1 -Path "/usr/bin"
+    ./install.ps1 -Path $ohMyPoshPath
     Pop-Location
 }
 
 if (Get-Command pwsh -ErrorAction SilentlyContinue) {
     Push-Location pwsh
-    ./install.ps1 -Path "/usr/bin"
+    ./install.ps1 -Path $ohMyPoshPath
     Pop-Location
 }
 
 if (Get-Command zsh -ErrorAction SilentlyContinue) {
     Push-Location zsh
-    ./install.ps1 -Path "/usr/bin"
+    ./install.ps1 -Path $ohMyPoshPath
     Pop-Location
 }
