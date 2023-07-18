65f87fa1873aeb28c096d315859dc590d12db5cc
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-07-18 04:17:18 +0000

    moved install.sh to .bin dir

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,7 +2,7 @@ git clone --bare https://github.com/WolfBublitz/Dotfiles.git $HOME/.dotfiles
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
-dotfiles checkout git-bare-test
-
 dotfiles config --local status.showUntrackedFiles no
+
+dotfiles checkout git-bare-test
 
