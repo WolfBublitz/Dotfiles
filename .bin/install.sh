8e6fdee59197de96a40d1ac8a3e842427fef8149
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-07-18 04:50:25 +0000

    updated install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,7 +2,9 @@
 
 git clone --bare https://github.com/WolfBublitz/Dotfiles.git $HOME/.dotfiles
 
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
 
 dotfiles config --local status.showUntrackedFiles no
 
