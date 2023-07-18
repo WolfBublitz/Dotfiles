43ea9a8223fc735dfc865275e8c95b22fa215872
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-07-18 04:03:47 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,7 @@
 git clone --bare https://github.com/WolfBublitz/Dotfiles.git $HOME/.dotfiles
 
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-}
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
 
-mkdir -p .dotfiles-backup
-
-dotfiles checkout
-
-if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
+dotfiles config --local status.showUntrackedFiles no
 
 dotfiles checkout git-bare-test
-
-dotfiles dotfiles status.showUntrackedFiles no
