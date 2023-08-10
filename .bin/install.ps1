98b1fb32ef0fb05e548dd169af0754ebc47ebe45
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-08-10 05:53:20 +0200

    Update install.ps1

--- .bin/install.ps1
+++ .bin/install.ps1
@@ -1,1 +1,9 @@
+git clone --bare https://github.com/WolfBublitz/Dotfiles.git $HOME/.dotfiles
 
+function dotfiles {
+    git --git-dir=$HOME/.dotfiles --work-tree=$HOME @args
+}
+
+dotfiles config --local status.showUntrackedFiles no
+
+dotfiles checkout --force
