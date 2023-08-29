2277e885a13c5631269467c8dfbeec12e2e3b6ce
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-08-29 06:33:12 +0200

    Update install.ps1

--- .bin/install.ps1
+++ .bin/install.ps1
@@ -9,9 +9,12 @@ dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout --force
 
 if ($IsWindows) {
+
+    # backing up current $PROFILE if exists
     if (Test-Path -Path $Profile -PathType Leaf) {
         Move-Item -Path $Profile -Destination $PROFILE.$(get-date -f yyyyMMdd)
     }
 
+    # referencing our $PROFILE 
     echo ". $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" >> $PROFILE
 }
