db2d454cad029020f8df2222e15ed1852fb23d5c
Wolf Bublitz <w.bublitz@krohne.com>
2023-08-29 07:30:16 +0200

    added Window Terminal Settings

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,10 @@ if ($hostname -eq 'ConsoleHost' -or $hostname -eq 'Visual Studio Code Host' ) {
   oh-my-posh init pwsh --config ~/.oh-my-posh/theme.omp.json | Invoke-Expression
 }
 
+function dotfiles {
+    git --git-dir=$HOME/.dotfiles --work-tree=$HOME @args
+}
+
 . $PSSCRIPTROOT/Aliases.ps1
 . $PSSCRIPTROOT/Functions.ps1
 . $PSSCRIPTROOT/PSReadLine.ps1
