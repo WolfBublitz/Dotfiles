b70df2647e8dc91718de15ca45383c8c47d669de
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-10-16 01:16:50 +0200

    zsh plugins are now only installed if zsh is available

--- .bin/install.sh
+++ .bin/install.sh
@@ -10,5 +10,8 @@ dotfiles config --local status.showUntrackedFiles no
 
 dotfiles checkout --force
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+if [ -x "$(command -v zsh)" ]; then
+   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+   git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/zsh-autocomplete
+fi
