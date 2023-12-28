ce46a5f3255bc607c126b4295c1ab486fd04dbc6
Wolf Bublitz <wolf.bublitz@gmail.com>
2023-12-28 19:29:54 +0100

    Update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -16,7 +16,7 @@ if [ -x "$(command -v zsh)" ]; then
    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/zsh-autocomplete
 fi
 
-declare -a tools = (neofetch htop vim)
+declare -a tools=(neofetch htop vim)
 
 for tool in "${tools[@]}"
 do
