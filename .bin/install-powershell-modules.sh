fb6684a32f689aaff61469a8a77f69df1c777013
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-01-16 10:33:34 +0100

    Update install-powershell-modules.sh

--- .bin/install-powershell-modules.sh
+++ .bin/install-powershell-modules.sh
@@ -2,3 +2,4 @@
 
 pwsh -c "Install-Module -Name PSReadLine -AllowClobber -Force"
 pwsh -c "Install-Module -Name CompletionPredictor -Force"
+pwsh -c "Install-Module -Name PwshSpectreConsole  -Force"
