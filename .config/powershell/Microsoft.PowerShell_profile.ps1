44566792f6dd0c2b4a68cf97cd2a078ee25c6464
Wolf Bublitz <w.bublitz@krohne.com>
2024-01-17 13:52:41 +0100

    added zip extension

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -114,46 +114,7 @@ if ($hostname -eq 'ConsoleHost' -or $hostname -eq 'Visual Studio Code Host' ) {
     }
   }
 
-  # creates a zip file from a file or folder
-  function Compress-Zip {
-    param(
-      [Parameter(Mandatory = $true)][string]$Path,
-      [Parameter(Mandatory = $false)][string]$DestinationPath = $null,
-      [Parameter(Mandatory = $false)][string]$CompressionLevel = "Fastest")
-
-    if ($DestinationPath -eq $null) {
-      $DestinationPath = "$($Path).zip"
-    }
-
-    $compress = @{
-      Path             = $Path
-      CompressionLevel = $CompressionLevel
-      DestinationPath  = $DestinationPath
-    }
-
-    Compress-Archive @compress
-  }
-
-  # expands a zip file to a folder
-  function Expand-Zip {
-    param(
-      [Parameter(Mandatory = $true)][string]$Path,
-      [Parameter(Mandatory = $false)][string]$DestinationPath = $null)
-
-    if ([System.String]::IsNullOrEmpty($DestinationPath)) {
-      $fileName = [System.IO.Path]::GetFileNameWithoutExtension($Path)
-      $filePath = [System.IO.Path]::GetDirectoryName($Path)
-
-      $DestinationPath = Join-Path  -Path $filePath -ChildPath $fileName
-    }
-
-    $expand = @{
-      Path            = $Path
-      DestinationPath = $DestinationPath
-    }
-
-    Expand-Archive @expand
-  }
+  
 
   # ┌──────────────────────────────────────────────────────────────────────┐
   # │ Extensions                                                           │
---
+++ .config/powershell/Zip.Extension.ps1
@@ -0,0 +1,67 @@
+<#
+    .SYNOPSIS
+        Creats a zip file from a folder.
+
+    .DESCRIPTION
+        Creats a zip file from a folder.
+
+    .PARAMETER Path
+        The path to the folder to zip.
+    
+    .PARAMETER DestinationPath
+        The path to the zip file to create.
+    
+    .PARAMETER CompressionLevel
+        The compression level to use (default: Fastest).
+#>
+function Compress-Zip {
+    param(
+        [Parameter(Mandatory = $true)][string]$Path,
+        [Parameter(Mandatory = $false)][string]$DestinationPath = $null,
+        [Parameter(Mandatory = $false)][string]$CompressionLevel = "Fastest")
+
+    if ($DestinationPath -eq $null) {
+        $DestinationPath = "$($Path).zip"
+    }
+
+    $compress = @{
+        Path             = $Path
+        CompressionLevel = $CompressionLevel
+        DestinationPath  = $DestinationPath
+    }
+
+    Compress-Archive @compress
+}
+
+<#
+    .SYNOPSIS
+        Extracts a zip file to a folder.
+    
+    .DESCRIPTION
+        Extracts a zip file to a folder.
+
+    .PARAMETER Path
+        The path to the zip file to extract.
+
+    .PARAMETER DestinationPath
+        The path to the folder to extract to.
+#>
+function Expand-Zip {
+    param(
+        [Parameter(Mandatory = $true)][string]$Path,
+        [Parameter(Mandatory = $false)][string]$DestinationPath = $null)
+
+    if ([System.String]::IsNullOrEmpty($DestinationPath)) {
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+        $filePath = [System.IO.Path]::GetDirectoryName($Path)
+
+        $DestinationPath = Join-Path  -Path $filePath -ChildPath $fileName
+    }
+
+    $expand = @{
+        Path            = $Path
+        DestinationPath = $DestinationPath
+    }
+
+    Expand-Archive @expand
+}
