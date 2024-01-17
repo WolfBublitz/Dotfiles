446692cc6cf57a6079eb10531333e4a0812ed969
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-01-17 05:59:59 +0100

    moved PSReadline to a separate extension file

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -22,6 +22,37 @@ if ($hostname -eq 'ConsoleHost' -or $hostname -eq 'Visual Studio Code Host' ) {
     }
   }
 
+  Function Add-Module {
+    param(
+      [Parameter(Mandatory = $true)][string]$Name
+    )
+    # check if module is imported
+    if (Get-Module | Where-Object { $_.Name -eq $Name }) {
+      return
+    }
+    else {
+
+      # check if module is not imported, but available on disk then import
+      if (Get-Module -ListAvailable | Where-Object { $_.Name -eq $Name }) {
+        Import-Module $Name
+      }
+      else {
+
+        # If module is not imported, not available on disk, but is in online gallery then install and import
+        if (Find-Module -Name $Name | Where-Object { $_.Name -eq $Name }) {
+          Install-Module -Name $Name -Force -Scope CurrentUser
+          Import-Module $Name
+        }
+        else {
+
+          # If the module is not imported, not available and not in the online gallery then abort
+          throw "Module $Name not imported, not available and not in an online gallery."
+        }
+      }
+    }
+  }
+
+
   # ┌──────────────────────────────────────────────────────────────────────┐
   # │ Shortcuts                                                            │
   # └──────────────────────────────────────────────────────────────────────┘
@@ -118,73 +149,6 @@ if ($hostname -eq 'ConsoleHost' -or $hostname -eq 'Visual Studio Code Host' ) {
   }
 
   # ┌──────────────────────────────────────────────────────────────────────┐
-  # │ PSReadLine                                                           │
-  # └──────────────────────────────────────────────────────────────────────┘
-
-  Import-Module PSReadLine
-
-  Import-Module CompletionPredictor
-
-  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-  Set-PSReadLineOption -PredictionViewStyle ListView
-  Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-  Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-
-  Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
-  Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
-  Set-PSReadLineKeyHandler -Key Alt+LeftArrow -Function ShellBackwardWord
-  Set-PSReadLineKeyHandler -Key Alt+RightArrow -Function ShellForwardWord
-
-  Set-PSReadlineOption -Colors @{
-    "Command"                = $PSStyle.Bold + $PSStyle.Foreground.FromRgb(0x0595f5)
-    "Comment"                = $PSStyle.Foreground.White
-    "ContinuationPrompt"     = '#7FFFD4'
-    "Default"                = '#50C878'
-    "Emphasis"               = $PSStyle.Bold + $PSStyle.Foreground.White
-    "Error"                  = '#7DF9FF'
-    "InlinePrediction"       = '#00A36C'
-    "Keyword"                = '#32CD32'
-    "ListPrediction"         = $PSStyle.Foreground.Green
-    "ListPredictionSelected" = $PSStyle.Background.Blue
-    "Member"                 = '#40E0D0'
-    "Number"                 = $PSStyle.Foreground.FromRgb(0x0595f5)
-    "Operator"               = $PSStyle.Foreground.White
-    "Parameter"              = $PSStyle.Foreground.BrightWhite
-    "Selection"              = $PSStyle.Foreground.White
-    "String"                 = $PSStyle.Foreground.BrightYellow
-    "Type"                   = '#C1E1C1'
-    "Variable"               = $PSStyle.Foreground.Green
-  }
-
-  Set-PSReadLineKeyHandler -Key Alt+Shift+B `
-    -BriefDescription BuildDotnetProject `
-    -LongDescription "Build the dotnet project in the current directory" `
-    -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet build")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-  }
-
-  Set-PSReadLineKeyHandler -Key Alt+Shift+T `
-    -BriefDescription RunDotnetTests `
-    -LongDescription "Run the dotnet tests in the current directory" `
-    -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet test")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-  }
-
-  Set-PSReadLineKeyHandler -Key Alt+Shift+P `
-    -BriefDescription RunDotnetTests `
-    -LongDescription "Run the dotnet tests in the current directory" `
-    -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("git pull")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-  }
-
-  # ┌──────────────────────────────────────────────────────────────────────┐
   # │ Extensions                                                           │
   # └──────────────────────────────────────────────────────────────────────┘
 
@@ -229,7 +193,6 @@ if ($hostname -eq 'ConsoleHost' -or $hostname -eq 'Visual Studio Code Host' ) {
   # ┌──────────────────────────────────────────────────────────────────────┐
   # │ Prompt                                                               │
   # └──────────────────────────────────────────────────────────────────────┘
-
   oh-my-posh init pwsh --config ~/.oh-my-posh/theme.omp.json | Invoke-Expression
 }
 
