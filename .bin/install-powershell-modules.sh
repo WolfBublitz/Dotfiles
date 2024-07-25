0455db116988f18a1dfa561cedc61fff8bb054b5
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-07-25 06:38:09 +0200

    refactoring

--- .bin/install-powershell-modules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pwsh -c "Install-Module -Name PSReadLine -AllowClobber -Force"
-pwsh -c "Install-Module -Name CompletionPredictor -Force"
-pwsh -c "Install-Module -Name PwshSpectreConsole  -Force"
