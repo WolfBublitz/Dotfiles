52847ad334a674569bfdbeb9f26aa2b0d622ad36
Wolf Bublitz <w.bublitz@krohne.com>
2025-02-28 15:24:33 +0000

    added fixes for older powershells

--- .config/powershell/PSReadLine.Extension.ps1
+++ .config/powershell/PSReadLine.Extension.ps1
@@ -4,8 +4,62 @@
 
 # Add-Module PSReadLine
 
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
+if ($host.Version.Major -ge 7 -and $host.Version.Minor -ge 3)
+{
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -PredictionViewStyle ListView
+
+    if ($host.Version.Major -ge 7 -and $host.Version.Minor -ge 3)
+    {
+        Set-PSReadlineOption -Colors @{
+            "Command"                = $PSStyle.Bold + $PSStyle.Foreground.FromRgb(0x0595f5)
+            "Comment"                = $PSStyle.Foreground.White
+            "ContinuationPrompt"     = '#7FFFD4'
+            "Default"                = '#50C878'
+            "Emphasis"               = $PSStyle.Bold + $PSStyle.Foreground.White
+            "Error"                  = '#7DF9FF'
+            "InlinePrediction"       = '#00A36C'
+            "Keyword"                = '#32CD32'
+            "ListPrediction"         = $PSStyle.Foreground.Green
+            "ListPredictionSelected" = $PSStyle.Background.Blue
+            "Member"                 = '#40E0D0'
+            "Number"                 = $PSStyle.Foreground.FromRgb(0x0595f5)
+            "Operator"               = $PSStyle.Foreground.White
+            "Parameter"              = $PSStyle.Foreground.BrightWhite
+            "Selection"              = $PSStyle.Foreground.White
+            "String"                 = $PSStyle.Foreground.BrightYellow
+            "Type"                   = '#C1E1C1'
+            "Variable"               = $PSStyle.Foreground.Green
+        }
+    }
+}
+else
+{
+    Set-PSReadLineOption -PredictionSource History
+
+    if ($host.Version.Major -ge 7 -and $host.Version.Minor -ge 3)
+    {
+        Set-PSReadlineOption -Colors @{
+            "Command"                = $PSStyle.Bold + $PSStyle.Foreground.FromRgb(0x0595f5)
+            "Comment"                = $PSStyle.Foreground.White
+            "ContinuationPrompt"     = '#7FFFD4'
+            "Default"                = '#50C878'
+            "Emphasis"               = $PSStyle.Bold + $PSStyle.Foreground.White
+            "Error"                  = '#7DF9FF'
+            "InlinePrediction"       = '#00A36C'
+            "Keyword"                = '#32CD32'
+            "Member"                 = '#40E0D0'
+            "Number"                 = $PSStyle.Foreground.FromRgb(0x0595f5)
+            "Operator"               = $PSStyle.Foreground.White
+            "Parameter"              = $PSStyle.Foreground.BrightWhite
+            "Selection"              = $PSStyle.Foreground.White
+            "String"                 = $PSStyle.Foreground.BrightYellow
+            "Type"                   = '#C1E1C1'
+            "Variable"               = $PSStyle.Foreground.Green
+        }
+    }
+}
+
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
@@ -15,26 +69,7 @@ Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
 Set-PSReadLineKeyHandler -Key Alt+LeftArrow -Function ShellBackwardWord
 Set-PSReadLineKeyHandler -Key Alt+RightArrow -Function ShellForwardWord
 
-Set-PSReadlineOption -Colors @{
-    "Command"                = $PSStyle.Bold + $PSStyle.Foreground.FromRgb(0x0595f5)
-    "Comment"                = $PSStyle.Foreground.White
-    "ContinuationPrompt"     = '#7FFFD4'
-    "Default"                = '#50C878'
-    "Emphasis"               = $PSStyle.Bold + $PSStyle.Foreground.White
-    "Error"                  = '#7DF9FF'
-    "InlinePrediction"       = '#00A36C'
-    "Keyword"                = '#32CD32'
-    "ListPrediction"         = $PSStyle.Foreground.Green
-    "ListPredictionSelected" = $PSStyle.Background.Blue
-    "Member"                 = '#40E0D0'
-    "Number"                 = $PSStyle.Foreground.FromRgb(0x0595f5)
-    "Operator"               = $PSStyle.Foreground.White
-    "Parameter"              = $PSStyle.Foreground.BrightWhite
-    "Selection"              = $PSStyle.Foreground.White
-    "String"                 = $PSStyle.Foreground.BrightYellow
-    "Type"                   = '#C1E1C1'
-    "Variable"               = $PSStyle.Foreground.Green
-}
+
 
 Set-PSReadLineKeyHandler -Key Alt+Shift+B `
     -BriefDescription BuildDotnetProject `
