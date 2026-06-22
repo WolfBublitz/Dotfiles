d6b81d74879bfd97e0ab4f835d3e08e77358dd0c
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-06-22 22:06:43 +0200

    Remove .config/JiraSettings.json

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+oh-my-posh init fish --config ~/jandedobbeleer.omp.json | source
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/wolf/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
