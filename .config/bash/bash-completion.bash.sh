741cc11bca41fca5394ea2748deadd876a351a6a
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-07-06 21:47:54 +0200

    Added 4 file(s): - aliases.bash.sh - bash-completion.bash.sh - fastfetch.bash.sh - oh-my-posh.bash.sh

---
+++ .config/bash/bash-completion.bash.sh
@@ -0,0 +1,3 @@
+if test -f "/etc/profile.d/bash_completion.sh"; then
+	source /etc/profile.d/bash_completion.sh
+fi
---
+++ .config/bash/oh-my-posh.bash.sh
@@ -0,0 +1,1 @@
+eval "$(oh-my-posh init bash --config $HOME/.oh-my-posh/theme.omp.json)"
